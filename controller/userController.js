1f02cb2e74db0bab02f52a86f6eec1de15cc99f3
Titas Chowdhury <titaschowdhury3gmail.com>
2023-01-24 17:59:39 +0530

    update

--- controller/userController.js
+++ controller/userController.js
@@ -1,4 +1,5 @@
 
+
 const users = [];
 
 module.exports.signup=(req, res)=>{
@@ -9,4 +10,19 @@ module.exports.signup=(req, res)=>{
     users.push(req.body);
     return res.status(200).send();
 }
+
 //http://localhost:4000/api/user/signup
+
+module.exports.signin=(req, res)=>{
+    let user = req.body;
+    if(!user.email || !user.password){
+        return res.status(400).send("Email and Password is required");
+    }
+    let foundUser = users.find(i=> i.email==user.email && i.password==user.password);
+    if(!foundUser){
+        return res.status(400).send("Email or Password is incorrect");
+    }else{
+        return res.status(200).send();
+    }
+    
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,5 @@ const controller = require("../controller/userController");
 const router = express.Router();
 
 router.post("/signup", controller.signup);
-
+router.post("/signin", controller.signin);
 module.exports.router = router;
