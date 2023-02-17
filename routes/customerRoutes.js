f36b3297b51cd6310d0a677fa27569142c13fd3e
Titas Chowdhury <titaschowdhury3gmail.com>
2023-02-17 12:22:22 +0530

    commit all

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -4,9 +4,11 @@ const controller = require("../controller/customerController");
 
 const router = express.Router();
 
-router.get("/", controller.get);
+router.get("/", controller.getAll);
+router.get("/page/:page", controller.get);
 router.post("/", controller.add);
 router.put("/", controller.update)
+router.get("/:name", controller.getByName);
 router.delete("/:name", controller.delete);
 
 module.exports.router = router;
